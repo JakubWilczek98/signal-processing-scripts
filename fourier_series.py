c38268236150ff1c750e97fda3979b9d7979d7ef
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-11-16 21:56:56 +0100

    Added 6 more f series

--- fourier_series.py
+++ fourier_series.py
@@ -41,13 +41,85 @@ def prostokatny_unipolarny_2(A, time, freq, elements): #Prostokatny unipolarny w
     
     return signal
 
+def trojkatny_bipolarny_1(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    interwals = np.arange(3, 2 * elements - 1, 2)
+
+    sign = -1
+    signal = np.sin(omega * time)
+    for i in range(len(interwals)):
+        signal = signal + sign * (1 / (interwals[i])**2 * np.sin(interwals[i] * omega * time))
+        sign = sign * -1
+
+    signal = (8*A)/np.pi**2 * signal
+
+    return signal
+
+def trojkatny_bipolarny_2(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    interwals = np.arange(2, elements - 1, 1)
+
+    sign = -1
+    signal = np.sin(omega * time)
+    for i in range(len(interwals)):
+        signal = signal + sign * (1 / (interwals[i]) * np.sin(interwals[i] * omega * time))
+        sign = sign * -1
+
+    signal = (2*A)/np.pi * signal
+
+    return signal
+
+def trojkatny_unipolarny_1(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    signal = 0
+    for i in range(elements):
+        signal = signal + (1/(2*i+1)**2)*np.cos((2*i+1)*omega*time)
+
+    signal = A/2-(4*A)/np.pi**2 * signal
+
+    return signal
+
+def trojkatny_unipolarny_2(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    signal = 0
+    for i in range(elements):
+        i += 1
+        signal = signal + (np.sin(i*omega*time)/i)
+
+    signal = A/2-(A)/np.pi * signal
+
+    return signal
+
+def sinusoidalny_wyprostowany_dwupolowkowo(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    signal = 0
+    for i in range(elements):
+        i += 1
+        signal = signal + 1/(4*i**2-1)*np.cos(2*i*omega*time)
+
+    signal = (2*A)/np.pi-(4*A)/np.pi * signal
+
+    return signal
+
+def sinusoidalny_wyprostowany_jednopolowkowo(A, time, freq ,elements):
+    omega = 2 * np.pi * freq
+    signal = 0
+    for i in range(elements):
+        i += 1
+        signal = signal + 1/(4*i**2-1)*np.cos(2*i*omega*time)
+
+    signal = A/np.pi+A/2*np.sin(omega*time)-(2*A)/np.pi * signal
+
+    return signal
+
+
 
 
 
 if __name__ == "__main__":
     A = 0.5
     f = 1
-    fs = 10000
+    fs = 50
     start_time = 0
     end_time = 3
     step_time = 1/fs
@@ -57,15 +129,54 @@ if __name__ == "__main__":
     
     #prostokatny bipolarny
     prostokatny_bipolarny = prostokatny_bipolarny(A, time, f, elements)
+    plt.figure(1)
     plt.plot(time, prostokatny_bipolarny)
     plt.show()
     
     #Prostokatny unipolarny z wypelnieniem 1/2
     prostokatny_unipolarny_1 = prostokatny_unipolarny_1(A, time, f, elements)
+    plt.figure(2)
     plt.plot(time, prostokatny_unipolarny_1)
     plt.show()
     
     #Prostokatny unipolarny z wypelnieniem dowolonym
+    plt.figure(3)
     prostokatny_unipolarny_2 = prostokatny_unipolarny_2(A, time, f, elements)
     plt.plot(time, prostokatny_unipolarny_2)
+    plt.show()
+
+    #Trojkatny bipolarny 1
+    trojkatny_bipolarny_1 = trojkatny_bipolarny_1(A, time, f, elements)
+    plt.figure(4)
+    plt.plot(time, trojkatny_bipolarny_1)
+    plt.show()
+
+    #Trojkatny bipolarny 2
+    trojkatny_bipolarny_2 = trojkatny_bipolarny_2(A, time, f, elements)
+    plt.figure(5)
+    plt.plot(time, trojkatny_bipolarny_2)
+    plt.show()
+
+    #Trojkatny unipolarny 1
+    trojkatny_unipolarny_1 = trojkatny_unipolarny_1(A, time, f, elements)
+    plt.figure(6)
+    plt.plot(time, trojkatny_unipolarny_1)
+    plt.show()
+
+    #Trojkatny unipolarny 2
+    trojkatny_unipolarny_2 = trojkatny_unipolarny_2(A, time, f, elements)
+    plt.figure(7)
+    plt.plot(time, trojkatny_unipolarny_2)
+    plt.show()
+
+    #Sinusoidalny wyprostowany dwupolowkowo
+    sinusoidalny_wyprostowany_dwupolowkowo = sinusoidalny_wyprostowany_dwupolowkowo(A, time, f, elements)
+    plt.figure(8)
+    plt.plot(time, sinusoidalny_wyprostowany_dwupolowkowo)
+    plt.show()
+
+    # Sinusoidalny wyprostowany jednopolowkowo
+    sinusoidalny_wyprostowany_jednopolowkowo = sinusoidalny_wyprostowany_jednopolowkowo(A, time, f, elements)
+    plt.figure(9)
+    plt.plot(time, sinusoidalny_wyprostowany_jednopolowkowo)
     plt.show()
