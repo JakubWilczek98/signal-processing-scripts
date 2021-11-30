9089fe5bbd2cd2c0fb482da7b6e0aaa026ffcb4f
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-11-30 18:53:53 +0100

    Adjustment

--- signal_characteristics.py
+++ signal_characteristics.py
@@ -3,8 +3,12 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-def draw_impulse(t,y,name):
-    #Impulse response 
+def tf(L,M):
+    return signal.TransferFunction(L,M)
+
+def draw_impulse(tf,name):
+    #Impulse response
+    t, y = signal.impulse(tf)
     plt.title("Impulse response - " + name)
     plt.plot(t,y)
     plt.xlabel("Time [s]")
@@ -12,8 +16,9 @@ def draw_impulse(t,y,name):
     plt.grid()
     plt.show()
     
-def draw_step(t,y,name):
+def draw_step(tf,name):
     #Step response 
+    t, y = signal.step(tf)
     plt.title("Step response - " + name)
     plt.plot(t,y)
     plt.xlabel("Time [s]")
@@ -29,20 +34,16 @@ if __name__ == "__main__":
     #Inertial system of the first order 
     L_rc = 1
     M_rc = np.array([R*C, 1])
-    tf_rc = signal.TransferFunction(L_rc, M_rc)
-    t1_im, y1_im = signal.impulse(tf_rc)
-    t1_stp, y1_stp = signal.step(tf_rc)
-    draw_impulse(t1_im, y1_im, "Inertial system of the first order")
-    draw_step(t1_stp, y1_stp, "Inertial system of the first order")
+    tf_rc = tf(L_rc,M_rc)
+    draw_impulse(tf_rc, "Inertial system of the first order")
+    draw_step(tf_rc, "Inertial system of the first order")
     
     
     #Differential system
     L_r = np.array([R*C,0])
     M_r = np.array([R*C,1])
-    t_r = signal.TransferFunction(L_r,M_r)
-    t2_im, y2_im = signal.impulse(tf_rc)
-    t2_stp, y2_stp = signal.step(tf_rc)
-    draw_impulse(t2_im, y2_im, "Differential system")
-    draw_step(t2_stp, y2_stp, "Differential system")
+    tf_r = tf(L_r,M_r)
+    draw_impulse(tf_r, "Differential system")
+    draw_step(tf_r, "Differential system")
     
     
